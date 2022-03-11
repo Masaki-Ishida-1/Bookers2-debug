4e70a4a5b6b94e6fff46142177a1a6fa8b8b853b
EC2 Default User <ec2-user@ip-172-31-10-232.ec2.internal>
2022-03-11 13:13:09 +0000

    DM機能

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
   get "search" => "searches#search"
   get 'chat/:id', to: 'chats#show', as: 'chat'
-  resouces :chats, only: [:create]
-
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
@@ -17,5 +15,6 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  resources :chats, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
